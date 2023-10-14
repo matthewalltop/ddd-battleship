930610c1c10bfcfaaab8d0788ad861b515beacf8
Matthew Alltop <balltop729@gmail.com>
2023-10-14 15:43:23 -0400

    Pipeline formatting

--- src/ddd.battleship.domain/Game.cs
+++ src/ddd.battleship.domain/Game.cs
@@ -1,6 +1,37 @@
 ﻿namespace ddd.battleship.domain;
 
-public class Game
-{
+using System.Collections.Generic;
+using ddd.battleship.core;
 
+public class Game : AggregateBase
+{
+	public override void ApplyEvent(object @event)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void ClearUncommittedEvents()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override IEnumerable<object> GetUncommittedEvents()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void LoadFromHistory(IEnumerable<object> history)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void LoadFromSnapshot(Snapshot snapshot)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override Snapshot TakeSnapshot()
+	{
+		throw new NotImplementedException();
+	}
 }
